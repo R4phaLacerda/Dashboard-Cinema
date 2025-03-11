c67b50f4f016851622c1593b7e155d75f7bba561
Raphaelle Lacerda <raphaellelacerda.tds2.senai@gmail.com>
2025-03-11 09:11:07 -0300

    feat: create header component

---
+++ src/components/header/index.jsx
@@ -0,0 +1,25 @@
+import styles from "../header/header.module.css";
+
+const Header = () => {
+    return (
+        <header class= {styles.mainHeader}>
+      <div class={styles.logo}>
+        <h1>Cinema Estrela</h1>
+      </div>
+      <nav class={styles.mainNav}>
+        <ul>
+          <li><a href="#" class="active">Dashboard</a></li>
+          <li><a href="#">Filmes</a></li>
+          <li><a href="#">Sessões</a></li>
+          <li><a href="#">Relatórios</a></li>
+        </ul>
+      </nav>
+      <div class={styles.userInfo}>
+        <span class={styles.userName}>Admin</span>
+        <div class={styles.userAvatar}></div>
+      </div>
+    </header>  
+    );
+};
+
+export default Header;
